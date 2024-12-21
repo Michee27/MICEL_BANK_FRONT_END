1edd85d2f6faaabf1cf1577277b74e8fbb6098e4
Michée Celestin <micheec@yahoo.fr>
2024-12-21 13:37:42 -0300

    Ajustando telas

--- src/js/routes/ProtectedRoute.js
+++ src/js/routes/ProtectedRoute.js
@@ -6,11 +6,10 @@ const ProtectedRoute = ({ children }) => {
     const { user } = useAuth();
 
     if (!user) {
-        // Redireciona para a página de login se não estiver autenticado
         return <Navigate to="/login" />;
     }
 
-    return children; // Renderiza o conteúdo protegido
+    return children;
 };
 
 export default ProtectedRoute;
