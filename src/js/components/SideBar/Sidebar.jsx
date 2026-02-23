af5ed1cdf8b876da2e3a238eb4e85e59ed98e5ae
unknown <micheec@yahoo.fr>
2026-02-23 19:17:25 -0300

    Fixing logomarca

--- src/js/components/SideBar/Sidebar.jsx
+++ src/js/components/SideBar/Sidebar.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { menuItems } from './utils/contants';
+import { menuItems } from "./utils/contants";
 
 const Sidebar = () => {
-
-    return (
-        <div className="sidebar">
-            <div className="logo">
-                DigiBank
-            </div>
-            {menuItems.map((item, index) => (
-                <a href={item.link} className="menu-item" key={index}>
-                    <i className={`fas ${item.icon}`}></i>
-                    {item.text}
-                </a>
-            ))}
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <div className="logo">MicelBank</div>
+      {menuItems.map((item, index) => (
+        <a href={item.link} className="menu-item" key={index}>
+          <i className={`fas ${item.icon}`}></i>
+          {item.text}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
